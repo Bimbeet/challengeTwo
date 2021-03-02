4e6d6284a43464e1de485fedd8c5bfc4b5b5192a
Bimbeet <coreythomas14@gmail.com>
2021-03-02 09:51:05 -0800

    wip1; volley requesting, location basics, singleton helper class

--- app/src/main/java/com/interapt/challengetwo/MainActivity.java
+++ app/src/main/java/com/interapt/challengetwo/MainActivity.java
@@ -1,18 +1,38 @@
 package com.interapt.challengetwo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.interapt.challengetwo.databinding.ActivityMainBinding;
 
+import org.json.JSONObject;
+
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
-    private final String apiKey = "AIzaSyA0yaW_SvDKM9ATvjIwCzJmrIKAPnzWbbQ";
+    private final String apiKey = "";
+    private int PROXIMITY_RADIUS = 1;
+    private double latitude;
+    private double longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(tView);
         Places.initialize(getApplicationContext(), apiKey);
         PlacesClient placesClient = Places.createClient(this);
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Consider calling ActivityCompat#requestPermissions here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission.
+            return;
+        }
+        Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        longitude = location.getLongitude();
+        latitude = location.getLatitude();
     }
     // https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=100&types=food&key=API_KEY
 
@@ -31,5 +62,64 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onSearchTouch(View v) {
+        if (!binding.radiusInput.getText().toString().isEmpty()) {
+            PROXIMITY_RADIUS = Integer.parseInt(binding.radiusInput.getText().toString());
+        }
+        Log.d("debugy", "PROXIMITY_RADIUS = " + PROXIMITY_RADIUS);
+        Intent i = getIntent();
+        StringBuilder googlePlacesUrl =
+                new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
+        googlePlacesUrl.append("location=").append(latitude).append(",").append(longitude);
+        googlePlacesUrl.append("&radius=").append(PROXIMITY_RADIUS);
+        googlePlacesUrl.append("&types=food&sensor=true");
+        googlePlacesUrl.append("&key=" + apiKey);
+
+        JsonObjectRequest request = new JsonObjectRequest(googlePlacesUrl.toString(), null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject result) {
+                        Log.i("debugy", "onResponse: Result = " + result.toString());
+//                        parseLocationResult(result);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("debugy", "onErrorResponse: Error = " + error);
+                        Log.e("debugy", "onErrorResponse: Error = " + error.getMessage());
+                    }
+                });
+
+        SearchHelper.getInstance(this).addToRequestQueue(request);
+    }
+//    private void parseLocationResult(final JSONObject result) {}
+}
+
+class SearchHelper {
+    private static SearchHelper INSTANCE;
+    private RequestQueue requestQueue;
+    private final Context context;
+
+    private SearchHelper(Context ctx) {
+        context = ctx;
+        requestQueue = getRequestQueue();
+    }
+
+    public static synchronized SearchHelper getInstance(Context ctx) {
+        if (INSTANCE == null) {
+            INSTANCE = new SearchHelper(ctx);
+        }
+        return (INSTANCE);
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return requestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
     }
 }
