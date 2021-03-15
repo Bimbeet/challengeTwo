bd86ef36a27b16ccf20ac87813c7865917ffe5c3
Bimbeet <coreythomas14@gmail.com>
2021-03-15 11:53:27 -0700

    wip4; photo req issue work, need image req and Gmap route still

--- app/src/main/java/com/interapt/challengetwo/MainActivity.java
+++ app/src/main/java/com/interapt/challengetwo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.interapt.challengetwo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,6 +10,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -21,6 +24,7 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.libraries.places.api.Places;
 import com.google.android.libraries.places.api.net.PlacesClient;
@@ -30,13 +34,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private ActivityMainBinding binding;
     private final String apiKey = "";
     // Remember not to save API key to repo!
     PlacesClient placesClient;
-    int PROXIMITY_RADIUS = 1;
+    int PROXIMITY_RADIUS = 1609;
     double latitude;
     double longitude;
 
@@ -53,13 +62,8 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             binding.searchButton.setVisibility(View.VISIBLE);
             binding.radiusInput.setVisibility(View.VISIBLE);
             binding.rInstruct.setVisibility(View.VISIBLE);
-//        } else if (shouldShowRequestPermissionRationale()) {
-//            binding.needLoc.setVisibility(View.VISIBLE);
-            // this would need work if we needed code for something weird, like the app not displaying the perm req on startup
         } else {
-            ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-                    99);
+            singleLocationRequest(null);
         }
     }
 
@@ -77,20 +81,29 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                         binding.searchButton.setVisibility(View.VISIBLE);
                         binding.radiusInput.setVisibility(View.VISIBLE);
                         binding.rInstruct.setVisibility(View.VISIBLE);
+
+                        binding.needLoc.setVisibility(View.INVISIBLE);
+                        binding.locPermButton.setVisibility(View.INVISIBLE);
                     } else {
                         binding.needLoc.setVisibility(View.VISIBLE);
-//                        ActivityCompat.requestPermissions(MainActivity.this,
-//                                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
-//                                99);
+                        binding.locPermButton.setVisibility(View.VISIBLE);
                     }
                 }
             }
         }
     }
 
+    public void singleLocationRequest(@Nullable View view) {
+        Log.d("debugy", "singleLocationRequest - requestPermissions hit");
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                99);
+        // on deny, currently button will not work in emulator testing
+    }
+
     public void onSearchTouch(View v) {
         if (!binding.radiusInput.getText().toString().isEmpty()) {
-            PROXIMITY_RADIUS = Integer.parseInt(binding.radiusInput.getText().toString());
+            PROXIMITY_RADIUS = 1609 * Integer.parseInt(binding.radiusInput.getText().toString());
             Log.d("debugy", "PROXIMITY_RADIUS = " + PROXIMITY_RADIUS);
         }
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
@@ -123,7 +136,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
         googlePlacesUrl.append("location=").append(latitude).append(",").append(longitude);
         googlePlacesUrl.append("&radius=").append(PROXIMITY_RADIUS);
-        googlePlacesUrl.append("&types=restaurant,bakery,cafe&sensor=true");
+        googlePlacesUrl.append("&types=restaurant&keyword=restaurant&sensor=true");
         googlePlacesUrl.append("&key=" + apiKey);
         Log.d("debugy", "Request = " + googlePlacesUrl.toString());
 
@@ -154,8 +167,24 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 //        preformRequest();
     }
 
+    public static Bitmap getBitmapFromURL(String src) {
+        try {
+            URL url = new URL(src);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            Log.e("Bitmap", "returned");
+            return myBitmap;
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("Exception", e.getMessage());
+            return null;
+        }
+    }
+
     private void parseLocationResult(final JSONObject result) {
-//        placesClient.fetchPlace();
         String name = null;
         String address = null;
         String photo_ref = null;
@@ -177,11 +206,42 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             JSONArray arr = result.getJSONArray("results");
             photo_ref = arr.getJSONObject(0).getJSONArray("photos").getJSONObject(0).getString("photo_reference");
             Log.d("debugy", "JSONobject: photo_ref - " + photo_ref);
+//            JSONArray photoArr = arr.getJSONObject(0).getJSONArray("photos");
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        if (!name.isEmpty() || !address.isEmpty()) {
+        if (photo_ref != null) {
+            try {
+                StringBuilder photoUrl =
+                        new StringBuilder("https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=");
+                photoUrl.append(photo_ref);
+                photoUrl.append("&key=" + apiKey);
+                Log.d("debugy", "Request = " + photoUrl);
+
+                StringRequest request = new StringRequest(Request.Method.GET, photoUrl.toString(),
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Log.i("debugy", "onResponse: response = " + response);
+//                                photoReqResults[0] = result;
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.e("debugy", "onErrorResponse: Error = " + error);
+                                Log.e("debugy", "onErrorResponse: Error = " + error.getMessage());
+                            }
+                        });
+                SearchHelper.getInstance(this).addToRequestQueue(request);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (name != null || address != null) {
             binding.searchButton.setVisibility(View.INVISIBLE);
             binding.radiusInput.setVisibility(View.INVISIBLE);
             binding.rInstruct.setVisibility(View.INVISIBLE);
@@ -189,6 +249,9 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             binding.nameDisplay.setVisibility(View.VISIBLE);
             binding.addressDisplay.setText(address);
             binding.addressDisplay.setVisibility(View.VISIBLE);
+//            Log.d("debugy", "urlBitmap - " + photoReqResults[0]);
+//            binding.placePhoto.setImageBitmap(getBitmapFromURL(photoUrl.toString()));
+//            binding.placePhoto.setVisibility(View.VISIBLE);
         }
     }
 }
