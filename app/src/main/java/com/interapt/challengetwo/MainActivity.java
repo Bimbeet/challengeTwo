54886f492e4a887b34d7ab756cb7533d119695a7
Bimbeet <coreythomas14@gmail.com>
2021-03-15 14:03:10 -0700

    wip5; photoReq working, still need G-map sendoff

--- app/src/main/java/com/interapt/challengetwo/MainActivity.java
+++ app/src/main/java/com/interapt/challengetwo/MainActivity.java
@@ -4,14 +4,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -24,9 +22,17 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.PhotoMetadata;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.FetchPhotoRequest;
+import com.google.android.libraries.places.api.net.FetchPhotoResponse;
+import com.google.android.libraries.places.api.net.FetchPlaceRequest;
+import com.google.android.libraries.places.api.net.FetchPlaceResponse;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.interapt.challengetwo.databinding.ActivityMainBinding;
 
@@ -34,18 +40,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LocationListener {
     private ActivityMainBinding binding;
-    private final String apiKey = "";
+    private final String apiKey = "AIzaSyBcwe1M5pUtNfkZqZ3SSerMNF9oD3Tvczc";
     // Remember not to save API key to repo!
     PlacesClient placesClient;
-    int PROXIMITY_RADIUS = 1609;
+    int PROXIMITY_RADIUS = 800;
     double latitude;
     double longitude;
 
@@ -57,8 +60,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         setContentView(tView);
         Places.initialize(getApplicationContext(), apiKey);
         placesClient = Places.createClient(this);
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
             binding.searchButton.setVisibility(View.VISIBLE);
             binding.radiusInput.setVisibility(View.VISIBLE);
             binding.rInstruct.setVisibility(View.VISIBLE);
@@ -76,7 +78,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 String permission = permissions[i];
                 int grantResult = grantResults[i];
 
-                if (permission.equals(Manifest.permission.ACCESS_FINE_LOCATION) || permission.equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                if (permission.equals(Manifest.permission.ACCESS_COARSE_LOCATION)) {
                     if (grantResult == PackageManager.PERMISSION_GRANTED) {
                         binding.searchButton.setVisibility(View.VISIBLE);
                         binding.radiusInput.setVisibility(View.VISIBLE);
@@ -96,15 +98,19 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void singleLocationRequest(@Nullable View view) {
         Log.d("debugy", "singleLocationRequest - requestPermissions hit");
         ActivityCompat.requestPermissions(MainActivity.this,
-                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
                 99);
         // on deny, currently button will not work in emulator testing
     }
 
     public void onSearchTouch(View v) {
         if (!binding.radiusInput.getText().toString().isEmpty()) {
-            PROXIMITY_RADIUS = 1609 * Integer.parseInt(binding.radiusInput.getText().toString());
-            Log.d("debugy", "PROXIMITY_RADIUS = " + PROXIMITY_RADIUS);
+            if ((1609 * Integer.parseInt(binding.radiusInput.getText().toString())) < 50000) {
+                PROXIMITY_RADIUS = 1609 * Integer.parseInt(binding.radiusInput.getText().toString());
+            } else {
+                PROXIMITY_RADIUS = 48000;
+            }
+            Log.d("debugy", "PROXIMITY_RADIUS in meters = " + PROXIMITY_RADIUS);
         }
         LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         try {
@@ -113,8 +119,7 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             Log.d("debugy", "location_services Exception throw : " + ex.toString());
         }
         // although we did this check already, the app seems to require it when getting the getLastKnownLocation
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
 //        try {
@@ -167,91 +172,97 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 //        preformRequest();
     }
 
-    public static Bitmap getBitmapFromURL(String src) {
-        try {
-            URL url = new URL(src);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setDoInput(true);
-            connection.connect();
-            InputStream input = connection.getInputStream();
-            Bitmap myBitmap = BitmapFactory.decodeStream(input);
-            Log.e("Bitmap", "returned");
-            return myBitmap;
-        } catch (IOException e) {
-            e.printStackTrace();
-            Log.e("Exception", e.getMessage());
-            return null;
-        }
-    }
-
     private void parseLocationResult(final JSONObject result) {
         String name = null;
         String address = null;
         String photo_ref = null;
+        String place_id = null;
         try {
-            JSONArray arr = result.getJSONArray("results");
-            name = arr.getJSONObject(0).getString("name");
-            Log.d("debugy", "JSONobject: name - " + name);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            JSONArray arr = result.getJSONArray("results");
-            address = arr.getJSONObject(0).getString("formatted_address");
-            Log.d("debugy", "JSONobject: address - " + address);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        try {
-            JSONArray arr = result.getJSONArray("results");
-            photo_ref = arr.getJSONObject(0).getJSONArray("photos").getJSONObject(0).getString("photo_reference");
-            Log.d("debugy", "JSONobject: photo_ref - " + photo_ref);
-//            JSONArray photoArr = arr.getJSONObject(0).getJSONArray("photos");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        if (photo_ref != null) {
+            JSONArray placesResultsArray = result.getJSONArray("results");
             try {
-                StringBuilder photoUrl =
-                        new StringBuilder("https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=");
-                photoUrl.append(photo_ref);
-                photoUrl.append("&key=" + apiKey);
-                Log.d("debugy", "Request = " + photoUrl);
-
-                StringRequest request = new StringRequest(Request.Method.GET, photoUrl.toString(),
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                Log.i("debugy", "onResponse: response = " + response);
-//                                photoReqResults[0] = result;
-                            }
-                        },
-                        new Response.ErrorListener() {
-                            @Override
-                            public void onErrorResponse(VolleyError error) {
-                                Log.e("debugy", "onErrorResponse: Error = " + error);
-                                Log.e("debugy", "onErrorResponse: Error = " + error.getMessage());
-                            }
-                        });
-                SearchHelper.getInstance(this).addToRequestQueue(request);
-
-            } catch (Exception e) {
+                name = placesResultsArray.getJSONObject(0).getString("name");
+                Log.d("debugy", "JSONobject: name - " + name);
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
+            try {
+                place_id = placesResultsArray.getJSONObject(0).getString("place_id");
+                Log.d("debugy", "JSONobject: place_id - " + place_id);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                address = placesResultsArray.getJSONObject(0).getString("formatted_address");
+                Log.d("debugy", "JSONobject: address - " + address);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
+//        try {
+//            JSONArray arr = result.getJSONArray("results");
+//            photo_ref = arr.getJSONObject(0).getJSONArray("photos").getJSONObject(0).getString("photo_reference");
+//            Log.d("debugy", "JSONobject: photo_ref - " + photo_ref);
+////            JSONArray photoArr = arr.getJSONObject(0).getJSONArray("photos");
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//
+//        if (photo_ref != null) {
+//            try {
+//                StringBuilder photoUrl =
+//                        new StringBuilder("https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=");
+//                photoUrl.append(photo_ref);
+//                photoUrl.append("&key=" + apiKey);
+//                Log.d("debugy", "Request = " + photoUrl);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
 
-        if (name != null || address != null) {
-            binding.searchButton.setVisibility(View.INVISIBLE);
-            binding.radiusInput.setVisibility(View.INVISIBLE);
-            binding.rInstruct.setVisibility(View.INVISIBLE);
-            binding.nameDisplay.setText(name);
-            binding.nameDisplay.setVisibility(View.VISIBLE);
-            binding.addressDisplay.setText(address);
-            binding.addressDisplay.setVisibility(View.VISIBLE);
-//            Log.d("debugy", "urlBitmap - " + photoReqResults[0]);
-//            binding.placePhoto.setImageBitmap(getBitmapFromURL(photoUrl.toString()));
-//            binding.placePhoto.setVisibility(View.VISIBLE);
+        if (place_id != null) {
+            final FetchPlaceRequest placeRequest = FetchPlaceRequest.newInstance(place_id, Collections.singletonList(Place.Field.PHOTO_METADATAS));
+            placesClient.fetchPlace(placeRequest).addOnSuccessListener(new OnSuccessListener<FetchPlaceResponse>() {
+                @Override
+                public void onSuccess(FetchPlaceResponse response) {
+                    final Place place = response.getPlace();
+                    final List<PhotoMetadata> metadata = place.getPhotoMetadatas();
+                    if (metadata == null || metadata.isEmpty()) {
+                        Log.w("debugy", "No photo metadata.");
+                        return;
+                    }
+                    final PhotoMetadata photoMetadata = metadata.get(0);
+//                    final String attributions = photoMetadata.getAttributions();
+                    final FetchPhotoRequest photoRequest = FetchPhotoRequest.builder(photoMetadata).build();
+                    placesClient.fetchPhoto(photoRequest).addOnSuccessListener(new OnSuccessListener<FetchPhotoResponse>() {
+                        @Override
+                        public void onSuccess(FetchPhotoResponse fetchPhotoResponse) {
+                            Bitmap bitmap = fetchPhotoResponse.getBitmap();
+                            binding.placePhoto.setImageBitmap(bitmap);
+                            binding.placePhoto.setVisibility(View.VISIBLE);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            if (exception instanceof ApiException) {
+                                final ApiException apiException = (ApiException) exception;
+                                Log.e("debugy", "Place not found: " + exception.getMessage());
+                            }
+                        }
+                    });
+                }
+            });
+
+            if (name != null || address != null) {
+                binding.searchButton.setVisibility(View.INVISIBLE);
+                binding.radiusInput.setVisibility(View.INVISIBLE);
+                binding.rInstruct.setVisibility(View.INVISIBLE);
+                binding.nameDisplay.setText(name);
+                binding.nameDisplay.setVisibility(View.VISIBLE);
+                binding.addressDisplay.setText(address);
+                binding.addressDisplay.setVisibility(View.VISIBLE);
+            }
         }
     }
 }
